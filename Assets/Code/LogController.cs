078f33cb3ba780f962ecacd4e0b0c3c2fc56c686
Rita0102(U10916037) <104476158+rita0102@users.noreply.github.com>
2023-10-17 23:46:54 +0800

    log

--- Assets/Code/LogController.cs
+++ Assets/Code/LogController.cs
@@ -14,37 +14,59 @@ public class LogController : MonoBehaviour
     public Text LogText;
     public TextAsset textFile;
     public int logindex = 0;
+    public int oldlogindex = 0;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        logindex = PlayerPrefs.GetInt("logindex");
+        /*
+        //logindex = PlayerPrefs.GetInt("logindex");
+
+        getLogindex();
 
         LogText.text = "";
         //LogText.text += logindex + "\n";              //代刚ノ
-       // LogText.text += dialog.textList[0] + "\n";     //代刚ノ
-       // LogText.text += dialog.textList[1] + "\n";     //代刚ノ
-       // LogText.text += dialog.textList[2] + "\n";     //代刚ノ
+        // LogText.text += dialog.textList[0] + "\n";     //代刚ノ
+        // LogText.text += dialog.textList[1] + "\n";     //代刚ノ
+        // LogText.text += dialog.textList[2] + "\n";     //代刚ノ
 
+        
+        for (int i = 0; i < logindex; i++)
+        {
+            LogText.text += dialog.textList[i] + "\n";
+        }*/
+    }
 
+    void OnEnable()
+    {
+        logindex = PlayerPrefs.GetInt("logindex");
+        LogText.text = "";
         for (int i = 0; i < logindex; i++)
         {
             LogText.text += dialog.textList[i] + "\n";
         }
     }
 
+
     /*
-    void Awake()
-    {
-        dialog.getTextFromFile(textFile);
-    }
-    */
     // Update is called once per frame
     void Update()
     {
         
-    }
+        int logindex2 = PlayerPrefs.GetInt("logindex");
+        if(logindex2 != logindex)
+        {
+            logindex = logindex2;
+
+            LogText.text = "";
+
+            for (int i = 0; i < logindex; i++)
+            {
+                LogText.text += dialog.textList[i] + "\n";
+            }
+        }
+    }*/
 
 }
