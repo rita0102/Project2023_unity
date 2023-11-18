bcfa10867ea103b24864044a6e8ddd287859520f
yongxin311 <63036047+yongxin311@users.noreply.github.com>
2023-11-19 05:10:38 +0800

    書櫃互動(碰到上面會有按鍵提示)

--- Assets/Code/Sign.cs
+++ Assets/Code/Sign.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Sign : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+
+    public Transform playerTrans;
+
+    public GameObject signSprite;
+
+    private bool canPress;
+
+
+    private void Awake()
     {
-        
+        anim = signSprite.GetComponent<Animator>();
+        anim.Play("keyboard");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        signSprite.GetComponent<SpriteRenderer>().enabled=canPress;
+        signSprite.transform.localScale = playerTrans.localScale;
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable")) {
+            canPress = true;
+        }
+    }
+    //當人物停在碰觸到物件(trigger)時觸發，碰到物件tag為interactable時，本來隱藏的物件顯示出來
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canPress = false;
     }
 }
